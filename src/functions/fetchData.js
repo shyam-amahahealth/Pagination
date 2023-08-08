7e3e85465429b01622b0539c4d584d44d3485ddb
shyam-amahahealth <shyam@amahahealth.com>
2023-08-08 17:44:40 +0530

    Error handeling on API fetch failure

--- src/functions/fetchData.js
+++ src/functions/fetchData.js
@@ -1,8 +1,20 @@
 const fetchData = async (page) => {
-  const response = await fetch(
-    `https://api.theinnerhour.com/v1/customers/resources/articles/list?page=${page}&limit=15`
-  );
-  const data = await response.json();
-  return data.data;
+  try {
+    const response = await fetch(
+      `https://api.theinnerhour.com/v1/customers/resources/articles/list?page=${page}&limit=15`
+    );
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const data = await response.json();
+    return data.data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    alert("An error occurred while fetching data. Please try again later.");
+    return [];
+  }
 };
+
 export default fetchData;
